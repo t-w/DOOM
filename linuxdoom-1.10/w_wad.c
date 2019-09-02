932c18559b0a80beb340c71ca7d2e4ea798b114a
t-m <tomas.wolak@gmail.com>
2019-09-02 19:13:08 +0200

    Commenting-out rcs_id (useless and causes lots of warnings in compilation).

--- linuxdoom-1.10/w_wad.c
+++ linuxdoom-1.10/w_wad.c
@@ -22,8 +22,7 @@
 //-----------------------------------------------------------------------------
 
 
-static const char
-rcsid[] = "$Id: w_wad.c,v 1.5 1997/02/03 16:47:57 b1 Exp $";
+//static const char rcsid[] = "$Id: w_wad.c,v 1.5 1997/02/03 16:47:57 b1 Exp $";
 
 
 #ifdef NORMALUNIX
