81b3dc92341803fe5e26083683dd6af48ccf9254
t-m <tomas.wolak@gmail.com>
2021-11-02 20:59:14 +0100

    p_setup: correct size of an array of pointers (pointer size is architecture-dependent).

---
+++ linuxdoom-1.10/i_cpuarch.h
@@ -0,0 +1,10 @@
+
+#ifndef __I_CPUARCH__
+#define __I_CPUARCH__
+
+//extern const int ptr_size = sizeof (void *); // sizeof(intptr_t);
+//#define CPU_ARCH_POINTER_SIZE = 
+//#define PTR_SIZE sizeof(intptr_t)
+#define PTR_SIZE sizeof(void *)
+
+#endif
