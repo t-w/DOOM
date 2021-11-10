a833502e7634bdcca299044a4e22669e64622571
t-m <tomas.wolak@gmail.com>
2021-11-10 11:56:50 +0100

    Including the SDL library header BEFORE main(), fixing #1

---
+++ linuxdoom-1.10/i_sdl.h
@@ -0,0 +1,13 @@
+
+#ifndef __I_SDL_H__
+#define __I_SDL_H__
+
+#ifdef __APPLE__
+    #include <SDL.h>
+    #include <SDL_mixer.h>
+#else
+    #include <SDL/SDL.h>
+    #include <SDL/SDL_mixer.h>
+#endif
+
+#endif
